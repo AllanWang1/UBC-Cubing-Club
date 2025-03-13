ef1e03890cba8fc4f1a12b8d07e6ce6264e63e47
allanwang <allan357161@gmail.com>
2025-03-13 13:13:31 -0700

    Added profile picture rendering, refactored logout button into dropdown menu

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,10 +2,21 @@ import { useEffect, useState } from "react";
 import { supabase } from "../SupabaseClient";
 import { useNavigate, Link } from "react-router-dom";
 
-import "../styles/Dashboard.css"
+import "../styles/Dashboard.css";
 
 const Dashboard = () => {
+  const getPublicURL = (path) => {
+    if (!path) return null;
+    const { data } = supabase.storage
+      .from("ProfilePictures")
+      .getPublicUrl(path);
+    return data.publicUrl;
+  };
+
   const [user, setUser] = useState(null);
+  const [avatarURL, setAvatarURL] = useState(getPublicURL("default1.png"));
+  const [isOpen, setIsOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const reloadPage = () => {
@@ -18,6 +29,9 @@ const Dashboard = () => {
         data: { user },
       } = await supabase.auth.getUser();
       setUser(user);
+      if (user.user_metadata?.profilePicURL) {
+        setAvatarURL(user.user_metadata.profilePicURL);
+      }
     };
     fetchUser();
   }, []);
@@ -29,16 +43,35 @@ const Dashboard = () => {
     reloadPage();
   };
 
+  const toggleIsOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="dashboard">
       {user ? (
-        <div className="dashboard-signed-in">
-          <h2>Welcome, {user.email}</h2>
-          <button onClick={handleLogout}>Logout</button>
+        <div className="dashboard-profile">
+          {user.user_metadata.full_name ? (
+            <h2>{user.user_metadata.full_name}</h2>
+          ) : (
+            <h2>{user.email}</h2>
+          )}
+          <div className="dashboard-profile-menu">
+            <img src={avatarURL} alt="Profile Picture" onClick={toggleIsOpen} />
+            {isOpen && (
+              <div className="dashboard-drop-down-menu">
+                <ul>
+                  <li>Profile Page</li>
+                  <li>Edit Profile</li>
+                  <li><button onClick={handleLogout}>Log Out</button></li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       ) : (
-        <Link to='/signin'>
-            <p>Sign In</p>
+        <Link to="/signin">
+          <p>Sign In</p>
         </Link>
       )}
     </div>
