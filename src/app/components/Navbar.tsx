303896e22023d29ffda9f64d89eda5c13060e8a2
allanwang <allan357161@gmail.com>
2025-05-02 15:13:35 -0700

    Migrated homepage, font color still needs adjustments. Font was chosen and layout.tsx was changed to match older version.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 
-import underline from "../../public/nav_underline.png";
 import "../styles/Navbar.css";
 import Dashboard from "./Dashboard";
 
@@ -20,31 +19,31 @@ const Navbar = () => {
           <Link href="/">
             <p onClick={() => setMenu("/")}>Home</p>
           </Link>
-          {menu === "/" && <Image src={underline.src} width={20} height={20} alt="underline" />}
+          {menu === "/" && <Image src="/nav_underline.png" width={20} height={20} alt="underline" />}
         </li>
         <li>
           <Link href="/aboutus">
             <p onClick={() => setMenu("/aboutus")}>About Us</p>
           </Link>
-          {menu === "/aboutus" && <Image src={underline.src} width={20} height={20} alt="" />}
+          {menu === "/aboutus" && <Image src="/nav_underline.png" width={20} height={20} alt="" />}
         </li>
         <li>
           <Link href="/leaderboard">
             <p onClick={() => setMenu("/leaderboard")}>Leaderboard</p>
           </Link>
-          {menu === "/leaderboard" && <Image src={underline.src} width={20} height={20} alt="" />}
+          {menu === "/leaderboard" && <Image src="/nav_underline.png" width={20} height={20} alt="" />}
         </li>
         <li>
           <Link href="/tournaments">
             <p onClick={() => setMenu("/tournaments")}>Tournaments</p>
           </Link>
-          {menu === "/tournaments" && <Image src={underline.src} width={20} height={20} alt="" />}
+          {menu === "/tournaments" && <Image src="/nav_underline.png" width={20} height={20} alt="" />}
         </li>
         <li>
           <Link href="/feed">
             <p onClick={() => setMenu("/feed")}>Feed</p>
           </Link>
-          {menu === "/feed" && <Image src={underline.src} width={20} height={20} alt="" />}
+          {menu === "/feed" && <Image src="/nav_underline.png" width={20} height={20} alt="" />}
         </li>
       </ul>
       <div className="profile">
