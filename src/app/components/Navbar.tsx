17bd0ed405c864405b44b1aaf880eb0dac648bba
allanwang <allan357161@gmail.com>
2025-06-07 12:29:09 -0700

    Modified navbar css and added tab icons.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,11 +10,19 @@ const Navbar = () => {
   const pathname = usePathname();
 
   const links = [
-    { href: "/", label: "Home" },
-    { href: "/aboutus", label: "About Us" },
-    { href: "/leaderboard", label: "Leaderboard" },
-    { href: "/meetings", label: "Meetings & Tournaments" },
-    { href: "/members", label: "Members" },
+    { href: "/", label: "Home", icon: "/navbar-icons/home.svg" },
+    { href: "/aboutus", label: "About Us", icon: "/navbar-icons/about.svg" },
+    {
+      href: "/leaderboard",
+      label: "Leaderboard",
+      icon: "/navbar-icons/leaderboard.svg",
+    },
+    {
+      href: "/meetings",
+      label: "Meetings",
+      icon: "/navbar-icons/meetings.svg",
+    },
+    { href: "/members", label: "Members", icon: "/navbar-icons/members.svg" },
   ];
 
   const isActive = (href: string) => {
@@ -27,12 +35,25 @@ const Navbar = () => {
       <ul className="navlinks">
         {links.map((link) => (
           <li key={link.href}>
-            <Link href={link.href}>
-              <p>{link.label}</p>
-            </Link>
-            {isActive(link.href) && (
-              <Image src="/nav_underline.png" width={20} height={20} alt="underline" />
-            )}
+            <div className="navlink">
+              <div className="navlinks-link-container">
+                <Image src={link.icon} width={20} height={20} alt="link icon" />
+                <Link href={link.href}>
+                  <p>{link.label}</p>
+                </Link>
+              </div>
+
+              {isActive(link.href) && (
+                <div className="underline">
+                  <Image
+                    src="/nav_underline.png"
+                    width={20}
+                    height={20}
+                    alt="underline"
+                  />
+                </div>
+              )}
+            </div>
           </li>
         ))}
       </ul>
@@ -43,4 +64,3 @@ const Navbar = () => {
   );
 };
 export default Navbar;
-
