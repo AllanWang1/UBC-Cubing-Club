34169b9dad1fbabb471aed4dab65aa4637b1a92f
allanwang <allan357161@gmail.com>
2026-07-20 19:00:30 -0700

    Added auto-deletion for the membership requests after validating changes to user_metadata were successful.

--- src/app/api/access-request/route.ts
+++ src/app/api/access-request/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "@/app/lib/SupabaseClient";
+import { createSupabaseServerClient } from "@/app/lib/SupabaseServer";
 
 export async function GET() {
   const { data, error } = await supabase.from("MemberRequest").select("*");
@@ -16,7 +17,7 @@ export async function POST(request: NextRequest) {
   if (!fullName || !email || !faculty || !birthDate || !UUID) {
     return NextResponse.json(
       { error: "Missing required fields" },
-      { status: 400 }
+      { status: 400 },
     );
   }
   const { data, error } = await supabase
@@ -38,4 +39,26 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
   return NextResponse.json(data, { status: 201 });
+}
+
+export async function DELETE(request: NextRequest) {
+  const { user_id } = await request.json();
+  if (!user_id) {
+    return NextResponse.json(
+      { error: "Missing UUID for access request deletion" },
+      { status: 400 },
+    );
+  }
+  const supabaseServer = await createSupabaseServerClient();
+  const { data, error } = await supabaseServer
+    .from("MemberRequest")
+    .delete()
+    .eq("user_id", user_id)
+    .select()
+    .single();
+  if (error) {
+    console.error("Error deleting access request: ", error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+  return NextResponse.json(data, { status: 200 });
 }
