911f68fae56f7ec4dcac609582c10795db1564e5
allanwang <allan357161@gmail.com>
2026-08-29 11:20:39 -0700

    Added edit profile page, and added button to the dashboard for redirecting to the edit profile page.

---
+++ src/app/(pages)/members/edit/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./MembersEdit.css"
+
+const MembersEdit = () => {
+  return (
+    <div className="members-edit">
+      <h2>Edit Profile</h2>
+    </div>
+  )
+}
+
+export default MembersEdit
