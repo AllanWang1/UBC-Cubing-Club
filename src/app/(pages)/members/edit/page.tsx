45cedd8310a4855414d64c5934ae6d366528fcc0
allanwang <allan357161@gmail.com>
2026-08-29 12:41:35 -0700

    Modified get Members API to be able to take a search param for user_id, and use that for filtering if it is provided.

--- src/app/(pages)/members/edit/page.tsx
+++ src/app/(pages)/members/edit/page.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+import { getUserId } from "@/app/lib/utils";
 import "./MembersEdit.css"
 
 const MembersEdit = () => {
+
+  // Need a way to obtain the current user's ID and display the corresponding Member
+  const [section, setSection] = useState<string>("avatar");
+  const [userId, setUserId] = useState<string>("");
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const uuid = await getUserId();
+      if (uuid) {
+        setUserId(uuid);
+      }
+    }
+    
+    fetchUserInfo();
+  }, [])
   return (
     <div className="members-edit">
       <h2>Edit Profile</h2>
